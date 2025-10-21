45713982d27abb67a99bcdd55dd48ed824d7dac8
SebastianL2024 <sebastian.larsson2@elev.ga.lbs.se>
2025-10-21 09:10:23 +0200

    push
    lektion

--- Paddle.cs
+++ Paddle.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace Labb2_ConsolePong
 {
-    internal class paddle
+    internal class Paddle
     {
+      public int x;
+      public int y;
+      public int size;
+       public Paddle(int startX,int startY,int paddleSize)
+        {
+            x = startX;
+            y = startY;
+            size = paddleSize;
+        }
+        void Move(int yAmount)
+        {
+            y += yAmount;
+        }
+        public void Draw()
+          
+        {
+           for (int i = 0; i )
+        }
+    
     }
 }
